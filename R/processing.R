41f3fc8d313a823d029b6f3fc3b3a0015eb4139e
csprock <csprock@gmail.com>
2022-06-23 18:48:13 -0600

    added helper functions to data processing

--- R/processing.R
+++ R/processing.R
@@ -21,13 +21,41 @@ clean_name <- function(str) {
     str_to_title()
 }
 
+country_name <- function(str) {
+  str_split_fixed(str, ":", n=2) %>%
+    .[, 1] %>%
+    str_trim() %>%
+    str_to_title()
+}
+
+
+
 eq_location_clean <- function(data) {
   data %>%
     mutate(
+      `Country Name` = country_name(`Location Name`),
+      `Country Name` = if_else(`Country Name` == "", NA_character_, `Country Name`),
       `Location Name` = clean_name(`Location Name`),
-      `Location Name` = if_else(`Location Name` == "", NA_character_, `Location Name`)
-    ) 
+      `Location Name` = if_else(`Location Name` == "", NA_character_, `Location Name`),
+    )
 }
-  
-  
+
+
+convert_lat_lon <- function(data) {
+  data %>%
+    mutate(
+      Latitude=as.numeric(Latitude),
+      Longitude=as.numeric(Longitude)
+    )
+}
+
+
+eq_clean_data <- function(data) {
+  data %>%
+    parse_date() %>%
+    eq_location_clean() %>%
+    convert_lat_lon()
+}
+
+
 
