b9388c883423c7d6e7b8736f12612645af71d125
csprock <csprock@gmail.com>
2022-06-25 13:08:19 -0600

    added geom_timeline_labels.R

---
+++ R/geom_timeline_labels.R
@@ -0,0 +1,55 @@
+
+draw_panel_timeline <- function(data, panel_scales, coord) {
+
+  data_trans <- coord$transform(data, panel_scales)
+
+  data_trans$y0 <- data_trans$y + 0.05
+
+
+  text_labels <- textGrob(
+    label = data_trans$label,
+    x=data_trans$x,
+    y=data_trans$y0,
+    just="left",
+    rot=45,
+    check.overlap = TRUE,
+    gp=gpar(fontsize=10)
+  )
+
+  print(data_trans$y0)
+  segments <- segmentsGrob(
+    x0=data_trans$x,
+    x1=data_trans$x,
+    y0=data_trans$y0,
+    y=data_trans$y,
+    default.units="npc"
+  )
+
+  return(gTree(children=gList(segments, text_labels)))
+}
+
+GeomLabelTimeline <- ggproto("GeomLabelTimeline", Geom,
+                        required_aes = c("x","y", "label"),
+                        draw_key = draw_key_point,
+                        draw_panel = draw_panel_timeline)
+
+
+geom_label_timeline <- function(
+  mapping=NULL,
+  data=NULL,
+  stat="identity",
+  position="identity",
+  na.rm=TRUE,
+  show.legend=NA,
+  inherit.aes=TRUE, ...) {
+  layer(
+    geom=GeomLabelTimeline,
+    mapping=mapping,
+    data=data,
+    stat=stat,
+    position=position,
+    show.legend=show.legend,
+    inherit.aes = inherit.aes,
+    params=list(na.rm=na.rm, ...)
+  )
+}
