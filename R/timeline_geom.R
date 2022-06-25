8ce4350737e9a78b6c78f201383b304ba6e11a55
csprock <csprock@gmail.com>
2022-06-25 10:35:05 -0600

    added magnitude scaling, but legend doesn't show correct size of circles

--- R/timeline_geom.R
+++ R/timeline_geom.R
@@ -1,9 +1,53 @@
 
-GeomTimeline <- ggproto("GeomTimeline", Geom,
-                         required_aes = c("x", "r"),
-                         default_aes = aes(color="grey", alpha=0.75, fill="blue"),
-                         draw_key = draw_key_polygon,
-                         draw_panel = draw_panel)
+
+
+
+create_timeline_grob <- function(data, panel_scales, coord) {
+
+
+  #data_trans <- data.frame(
+  #  x = data[["x"]],
+  #  y = data[["y"]]
+  #) %>% coord$transform(panel_scales)
+
+  data_trans <- coord$transform(data, panel_scales)
+
+  y_pos <- data_trans[["y"]][1]
+  base_horizontal_line <- linesGrob(x=c(0, 1), y=c(y_pos, y_pos))
+
+
+  circles <- circleGrob(
+    x=data_trans$x,
+    y=data_trans$y,
+    r=data_trans$size,
+    gp=gpar(
+      col="black",
+      fill=data_trans$fill,
+      alpha=0.5
+    )
+  )
+
+
+  return(gTree(children=gList(circles, base_horizontal_line)))
+
+  return(grobs)
+}
+
+
+draw_panel <- function(data, panel_scales, coord) {
+  if (isFALSE("y" %in% colnames(data))) {
+    data$y <- 0.5
+  }
+
+  timeline_grob <- create_timeline_grob(data, panel_scales, coord)
+  return(timeline_grob)
+}
+
+GeomTimeline <- ggproto("GeomTimeline", GeomPoint,
+                        required_aes = c("x", "size", "fill"),
+                        default_aes = aes(color="grey", alpha=0.75),
+                        draw_key = draw_key_point,
+                        draw_panel = draw_panel)
 
 
 geom_timeline <- function(
@@ -27,56 +71,12 @@ geom_timeline <- function(
 }
 
 
-
-create_timeline_grob <- function(data, panel_scales, coord) {
-
-
-  data_trans <- data.frame(
-    x = map_to_interval(data[["x"]], x_min=min(data$x), x_max=max(data$x), u=0.9, l=0.1),
-    y = data[["y"]],
-    radius = map_to_interval(data[["r"]], x_min=0, x_max=10, u=0.04, l=0.005)
-  ) #%>% coord$transform(panel_scales)
-
-  y_pos <- data_trans[["y"]][1]
-  base_horizontal_line <- linesGrob(x=c(0.1, 0.9), y=c(y_pos, y_pos))
-  print(y_pos)
-  # create circles
-  circles <- list()
-  for (i in 1:nrow(data_trans)) {
-    print(data_trans[["x"]][i])
-    circles[[i]] <- circleGrob(
-      x=data_trans[["x"]][i],
-      y=y_pos,
-      r=data_trans[["radius"]][i],
-      gp=gpar(
-        col="black",
-        fill=data[["fill"]][i],
-        alpha=0.5
-      )
-    )
-  }
-
-  circles[[length(circles)+1]] <- base_horizontal_line
-
-  grobs <- gTree(children=do.call(gList, circles))
-
-  return(grobs)
-}
-
-
-draw_panel <- function(data, panel_scales, coord) {
-  if (isFALSE("y" %in% colnames(data))) {
-    data$y <- 0.5
-  }
-
-  timeline_grob <- create_timeline_grob(data, panel_scales, coord)
-  return(timeline_grob)
-}
-
 ### test
 ca_data %>%
-  ggplot(mapping=aes(x=Year, r=Mag, fill=Deaths)) +
+  ggplot(mapping=aes(x=Year, size=Mag, fill=Deaths)) +
   geom_timeline() +
-  scale_fill_continuous()
+  scale_fill_continuous() +
+  labs(fill="#deaths", size="Magnitude") +
+  scale_radius(range=c(0, 0.1))
 
 
