1201248c322ed608aef23efe6a53207da97fefe6
csprock <csprock@gmail.com>
2022-06-27 18:47:31 -0600

    moved draw_panel functions to nest in the geom definitions

--- R/geom_timeline_labels.R
+++ R/geom_timeline_labels.R
@@ -1,42 +1,42 @@
 
-draw_panel_timeline <- function(data, panel_scales, coord) {
-
-  # filter to the top 5 for each country
-  data <- data %>%
-    dplyr::group_by(y) %>%
-    dplyr::top_n(x, n=data$n_max[1])
-
-  data_trans <- coord$transform(data, panel_scales)
-
-  data_trans$y0 <- data_trans$y + 0.05
-
-
-  text_labels <- grid::textGrob(
-    label = data_trans$label,
-    x=data_trans$x,
-    y=data_trans$y0,
-    just="left",
-    rot=45,
-    check.overlap = TRUE,
-    gp=grid::gpar(fontsize=10)
-  )
-
-  segments <- grid::segmentsGrob(
-    x0=data_trans$x,
-    x1=data_trans$x,
-    y0=data_trans$y0,
-    y=data_trans$y,
-    default.units="npc"
-  )
-
-  return(grid::gTree(children=grid::gList(segments, text_labels)))
-}
 
 GeomLabelTimeline <- ggplot2::ggproto("GeomLabelTimeline", ggplot2::Geom,
                         required_aes = c("x","y", "label"),
                         default_aes = c(n_max=5),
                         draw_key = ggplot2::draw_key_point,
-                        draw_panel = draw_panel_timeline)
+                        draw_panel = function(data, panel_scales, coord) {
+
+                          # filter to the top 5 for each country
+                          data <- data %>%
+                            dplyr::group_by(y) %>%
+                            dplyr::top_n(x, n=data$n_max[1])
+
+                          data_trans <- coord$transform(data, panel_scales)
+
+                          data_trans$y0 <- data_trans$y + 0.05
+
+
+                          text_labels <- grid::textGrob(
+                            label = data_trans$label,
+                            x=data_trans$x,
+                            y=data_trans$y0,
+                            just="left",
+                            rot=45,
+                            check.overlap = TRUE,
+                            gp=grid::gpar(fontsize=10)
+                          )
+
+                          segments <- grid::segmentsGrob(
+                            x0=data_trans$x,
+                            x1=data_trans$x,
+                            y0=data_trans$y0,
+                            y=data_trans$y,
+                            default.units="npc"
+                          )
+
+                          return(grid::gTree(children=grid::gList(segments, text_labels)))
+                        }
+                      )
 
 
 #' Add labels to timeline
--- R/timeline_geom.R
+++ R/timeline_geom.R
@@ -1,32 +1,28 @@
 
- draw_panel <- function(data, panel_scales, coord) {
-
-   data_trans <- coord$transform(data, panel_scales)
-
-   circles <- grid::circleGrob(
-     x=data_trans$x,
-     y=data_trans$y,
-     r=map_to_interval(data_trans$size, x_min=0, x_max=10, u=0.05, l=0),
-     gp=grid::gpar(
-       col=data_trans$color,
-       fill=data_trans$fill,
-       alpha=data_trans$alpha
-     )
-   )
-
-   return(grid::gTree(children=grid::gList(circles)))
-}
 
 GeomTimeline <- ggplot2::ggproto("GeomTimeline", ggplot2::GeomPoint,
                         required_aes = c("x","y", "size", "fill"),
                         default_aes = ggplot2::aes(color="grey", alpha=0.75),
                         draw_key = ggplot2::draw_key_point,
-                        draw_panel = draw_panel)
+                        draw_panel = function(data, panel_scales, coord) {
 
-#' A timeline geom
-#'
-#' Add a timeline to the earthquake data
-#'
+                          data_trans <- coord$transform(data, panel_scales)
+
+                          circles <- grid::circleGrob(
+                            x=data_trans$x,
+                            y=data_trans$y,
+                            r=map_to_interval(data_trans$size, x_min=0, x_max=10, u=0.05, l=0),
+                            gp=grid::gpar(
+                              col=data_trans$color,
+                              fill=data_trans$fill,
+                              alpha=data_trans$alpha
+                            )
+                          )
+
+                          return(grid::gTree(children=grid::gList(circles)))
+                        )
+
+
 #' @export
 geom_timeline <- function(
   mapping=NULL,
