a50a0ce23906142968bd65fae0cf96ee1d8aa711
csprock <csprock@gmail.com>
2022-07-02 10:47:40 -0600

    added documentation to the timeline geoms

--- R/timeline_geom.R
+++ R/timeline_geom.R
@@ -1,5 +1,20 @@
 
-
+#' Base class for geom_timeline
+#'
+#' @param required_aes the required asethetics for this geom are
+#' \enumerate{
+#'  \item x numeric representing the year of the earthquake
+#'  \item y factor representing the region. A new timeline will be plotted for each level of the factor
+#'  \item size a numeric controlling the size of the circle marks along the timeline
+#'  \item fill a numeric controlling the fill of the circle marks
+#' }
+#' @param default_aes default asthetics for this geom are
+#' \enumerate{
+#'  \item color, default color of the border of the circles
+#'  \item alpha, numeric between 0 and 1 which controls the transparency of circles
+#' }
+#'
+#' @seealso [geom_timeline()]
 GeomTimeline <- ggplot2::ggproto("GeomTimeline", ggplot2::GeomPoint,
                         required_aes = c("x","y", "size", "fill"),
                         default_aes = ggplot2::aes(color="grey", alpha=0.75),
@@ -22,7 +37,43 @@ GeomTimeline <- ggplot2::ggproto("GeomTimeline", ggplot2::GeomPoint,
                           return(grid::gTree(children=grid::gList(circles)))
                         )
 
-
+#' Create an earthquake timeline
+#'
+#' Creates a timeline of earthquakes for each specified country passed to the geom.
+#'
+#' This geom is for the visualization of the [NOAA earthquake data](https://www.ngdc.noaa.gov/hazel/view/hazards/earthquake/search). The
+#' geom displays a timeline where circles mark the year of an earthquake and whose radius is proportional
+#' to the magnitude and whose color fill intensity is proportional to the number of deaths. A factor can be supplied
+#' to the geom whose levels are countries and a separate timeline will be draw for each country. It is recommended
+#' that no more than three countries be visualized in this way to avoid crowding the figure.
+#'
+#' This geom is not to be used with the raw data from NOAA but must be first cleaned with the [eq_clean_data()] function.
+#'
+#' @param mapping A set of aesthetic mappings created by \code{aes()}
+#' @param data The data to be displayed in this layer
+#'
+#' @section Aesthetics
+#'
+#' The \code{geom_timeline()} understands the following aesthetics
+#' \itemize{
+#'  \item x numeric representing the year of the earthquake
+#'  \item y factor representing the region. A new timeline will be plotted for each level of the factor
+#'  \item size a numeric controlling the size of the circle marks along the timeline
+#'  \item fill a numeric controlling the fill of the circle marks
+#'  \item alpha optional numeric between 0 and 1 controlling the transparency of the circles
+#'  \item color optional character specifying the color of the circle borders
+#' }
+#'
+#' @seealso [eq_clean_data()], [geom_label_timeline()]
+#'
+#' @examples
+#' \dontrun{
+#'  data <- eq_clean(raw_data)
+#'  data %>%
+#'   ggplot(aes(x=Year, y=Country, size=Magnitude, fill=Deaths)) +
+#'   geom_timeline(alpha=0.5)
+#' }
+#'
 #' @export
 geom_timeline <- function(
   mapping=NULL,
